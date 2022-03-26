10d001f4640c983c476be85543f20412a04dbf5f
GAURAB <72376166+Gaurab4@users.noreply.github.com>
2022-03-26 20:44:19 +0530

    Add files via upload

---
+++ Unit_01/P2_Task01_KeywordsInJava.java
@@ -0,0 +1,23 @@
+package Unit_01;
+
+public class P2_Task01_KeywordsInJava {
+
+	public static void main(String[] args) {
+		//Syntax error on token "float",Invalid VariableDeclaratorid
+		int float=5;
+		try(float);
+	}
+	//Syntax error on token "float",Invalid VariableDeclaratorid
+	student double=new student();
+	//Syntax error on token "break",identifier expected
+	void try(int n) {
+		
+	}
+}
+class student{
+	int a=0;
+}
+//Syntax error on token "break",identifier expected
+class break{
+	float b=3;
+}
---
+++ Unit_01/P2_Task02_ConstantsInJava.java
@@ -0,0 +1,13 @@
+package Unit_01;
+
+public class P2_Task02_ConstantsInJava {
+
+	public static final double pi=3.14;
+	public static void main(String[] args) {
+		System.out.println(pi);
+		fun(pi);
+	}
+	static void fun(double x){
+		System.out.println(x);
+	}
+}
